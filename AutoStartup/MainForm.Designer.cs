1da885341731450dd68d9e75e660e8e58702cf6f
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2023-01-28 13:31:10 +0800

    添加项目文件。

---
+++ AutoStartup/MainForm.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace AutoStartup
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartUpSelector = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.OpenFileDialogBtn = new System.Windows.Forms.Button();
+            this.ProgramEnabled = new System.Windows.Forms.CheckBox();
+            this.ProgramArg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ProgramPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ProgramName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartupList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.EditBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.TestBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartUpSelector
+            // 
+            this.StartUpSelector.AutoSize = true;
+            this.StartUpSelector.Location = new System.Drawing.Point(12, 12);
+            this.StartUpSelector.Name = "StartUpSelector";
+            this.StartUpSelector.Size = new System.Drawing.Size(75, 21);
+            this.StartUpSelector.TabIndex = 0;
+            this.StartUpSelector.Text = "开机自启";
+            this.StartUpSelector.UseVisualStyleBackColor = true;
+            this.StartUpSelector.CheckedChanged += new System.EventHandler(this.StartUpSelector_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.OpenFileDialogBtn);
+            this.groupBox1.Controls.Add(this.ProgramEnabled);
+            this.groupBox1.Controls.Add(this.ProgramArg);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.ProgramPath);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ProgramName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 139);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "添加参数 (可拖动文件)";
+            // 
+            // OpenFileDialogBtn
+            // 
+            this.OpenFileDialogBtn.Location = new System.Drawing.Point(390, 51);
+            this.OpenFileDialogBtn.Name = "OpenFileDialogBtn";
+            this.OpenFileDialogBtn.Size = new System.Drawing.Size(75, 23);
+            this.OpenFileDialogBtn.TabIndex = 7;
+            this.OpenFileDialogBtn.Text = "浏览";
+            this.OpenFileDialogBtn.UseVisualStyleBackColor = true;
+            this.OpenFileDialogBtn.Click += new System.EventHandler(this.OpenFileDialog_Click);
+            // 
+            // ProgramEnabled
+            // 
+            this.ProgramEnabled.AutoSize = true;
+            this.ProgramEnabled.Location = new System.Drawing.Point(6, 109);
+            this.ProgramEnabled.Name = "ProgramEnabled";
+            this.ProgramEnabled.Size = new System.Drawing.Size(51, 21);
+            this.ProgramEnabled.TabIndex = 6;
+            this.ProgramEnabled.Text = "启用";
+            this.ProgramEnabled.UseVisualStyleBackColor = true;
+            // 
+            // ProgramArg
+            // 
+            this.ProgramArg.Location = new System.Drawing.Point(47, 80);
+            this.ProgramArg.Name = "ProgramArg";
+            this.ProgramArg.Size = new System.Drawing.Size(418, 23);
+            this.ProgramArg.TabIndex = 5;
+            this.ProgramArg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Program_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "参数:";
+            // 
+            // ProgramPath
+            // 
+            this.ProgramPath.Location = new System.Drawing.Point(47, 51);
+            this.ProgramPath.Name = "ProgramPath";
+            this.ProgramPath.Size = new System.Drawing.Size(337, 23);
+            this.ProgramPath.TabIndex = 3;
+            this.ProgramPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Program_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "路径:";
+            // 
+            // ProgramName
+            // 
+            this.ProgramName.Location = new System.Drawing.Point(47, 22);
+            this.ProgramName.Name = "ProgramName";
+            this.ProgramName.Size = new System.Drawing.Size(418, 23);
+            this.ProgramName.TabIndex = 1;
+            this.ProgramName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Program_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "名称:";
+            // 
+            // StartupList
+            // 
+            this.StartupList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.StartupList.FullRowSelect = true;
+            this.StartupList.Location = new System.Drawing.Point(12, 213);
+            this.StartupList.Name = "StartupList";
+            this.StartupList.Size = new System.Drawing.Size(471, 248);
+            this.StartupList.TabIndex = 2;
+            this.StartupList.UseCompatibleStateImageBehavior = false;
+            this.StartupList.View = System.Windows.Forms.View.Details;
+            this.StartupList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.StartupList_KeyDown);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "启用";
+            this.columnHeader1.Width = 25;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "名称";
+            this.columnHeader2.Width = 25;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "路径";
+            this.columnHeader3.Width = 25;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "参数";
+            this.columnHeader4.Width = 36;
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(12, 184);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddBtn.TabIndex = 3;
+            this.AddBtn.Text = "添加";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // EditBtn
+            // 
+            this.EditBtn.Location = new System.Drawing.Point(93, 184);
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.Size = new System.Drawing.Size(75, 23);
+            this.EditBtn.TabIndex = 4;
+            this.EditBtn.Text = "编辑";
+            this.EditBtn.UseVisualStyleBackColor = true;
+            this.EditBtn.Click += new System.EventHandler(this.EditBtn_Click);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(174, 184);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.DeleteBtn.TabIndex = 5;
+            this.DeleteBtn.Text = "删除";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "openFileDialog1";
+            this.OpenFileDialog.Filter = "可执行程序|*.exe|所有文件|*.*";
+            this.OpenFileDialog.Title = "选择需要启动程序的路径";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.StopBtn);
+            this.groupBox2.Controls.Add(this.StatusLabel);
+            this.groupBox2.Location = new System.Drawing.Point(12, 467);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(471, 64);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "状态";
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Enabled = false;
+            this.StopBtn.Location = new System.Drawing.Point(390, 26);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 23);
+            this.StopBtn.TabIndex = 1;
+            this.StopBtn.Text = "终止";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(10, 29);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(65, 17);
+            this.StatusLabel.TabIndex = 0;
+            this.StatusLabel.Text = "维护模式...";
+            // 
+            // TestBtn
+            // 
+            this.TestBtn.Location = new System.Drawing.Point(402, 184);
+            this.TestBtn.Name = "TestBtn";
+            this.TestBtn.Size = new System.Drawing.Size(75, 23);
+            this.TestBtn.TabIndex = 7;
+            this.TestBtn.Text = "测试";
+            this.TestBtn.UseVisualStyleBackColor = true;
+            this.TestBtn.Click += new System.EventHandler(this.TestBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(495, 543);
+            this.Controls.Add(this.TestBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.EditBtn);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.StartupList);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.StartUpSelector);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.DragLeave += new System.EventHandler(this.MainForm_DragLeave);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CheckBox StartUpSelector;
+        private GroupBox groupBox1;
+        private CheckBox ProgramEnabled;
+        private TextBox ProgramArg;
+        private Label label3;
+        private TextBox ProgramPath;
+        private Label label2;
+        private TextBox ProgramName;
+        private Label label1;
+        private ListView StartupList;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private Button AddBtn;
+        private Button EditBtn;
+        private Button DeleteBtn;
+        private Button OpenFileDialogBtn;
+        private OpenFileDialog OpenFileDialog;
+        private GroupBox groupBox2;
+        private Button StopBtn;
+        private Label StatusLabel;
+        private Button TestBtn;
+    }
+}
---
+++ AutoStartup/MainForm.cs
@@ -0,0 +1,349 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Diagnostics;
+
+namespace AutoStartup
+{
+    public partial class MainForm : Form
+    {
+        public static List<StartUpItem> StartUpItems { get; set; } = new();
+        public bool Maintance { get; set; }
+        public bool HasError { get; set; }
+        public string CurrentStatus
+        {
+            get => StatusLabel.Text;
+            set
+            {
+                if (InvokeRequired)
+                {
+                    Invoke(new MethodInvoker(() => StatusLabel.Text = value));
+                }
+                else
+                {
+                    StatusLabel.Text = value;
+                }
+            }
+        }
+        public class StartUpItem
+        {
+            public string Name { get; set; }
+            public string Path { get; set; }
+            public string Arg { get; set; }
+            public bool Enabled { get; set; }
+        }
+
+        public MainForm(bool v)
+        {
+            Maintance = v;
+            InitializeComponent();
+        }
+
+        public static void AddStartupProgram(string name, string path, bool add = true)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (rk == null)
+            {
+                MessageBox.Show("添加失败", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (add)
+                rk.SetValue(name, path);
+            else
+                rk.DeleteValue(name, false);
+        }
+
+        public bool IsStartup => Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true).GetValueNames().Any(x => x == "AutoStartup");
+        public bool Loaded { get; set; }
+        public bool StopFlag { get; set; }
+        public bool TestFlag { get; set; }
+
+        private void StartupList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartupList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                Debug.WriteLine("a");
+            }
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ProgramName.Text))
+            {
+                MessageBox.Show("名称无效", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!File.Exists(ProgramPath.Text))
+            {
+                MessageBox.Show("文件路径不存在", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (MessageBox.Show("确认要添加吗？", "Q", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+            {
+                return;
+            }
+            if (StartUpItems.Any(x => x.Path == ProgramPath.Text))
+            {
+                MessageBox.Show("存在路径相同的文件", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            StartUpItems.Add(new StartUpItem
+            {
+                Name = ProgramName.Text,
+                Path = ProgramPath.Text,
+                Arg = ProgramArg.Text,
+                Enabled = ProgramEnabled.Checked
+            });
+            File.WriteAllText("Config.json", JsonConvert.SerializeObject(StartUpItems, Formatting.Indented));
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            StartupList.Items.Clear();
+            for (int i = 0; i < StartupList.Columns.Count; i++)
+            {
+                StartupList.Columns[i].Width = -1;
+            }
+            foreach (var item in StartUpItems)
+            {
+                ListViewItem viewItem = new();
+                viewItem.SubItems[0].Text = item.Enabled ? "是" : "否";
+                viewItem.SubItems.Add(item.Name);
+                viewItem.SubItems.Add(item.Path);
+                viewItem.SubItems.Add(item.Arg);
+                StartupList.Items.Add(viewItem);
+            }
+            StartupList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void EditBtn_Click(object sender, EventArgs e)
+        {
+            if (StartupList.SelectedIndices.Count == 0)
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty(ProgramName.Text))
+            {
+                MessageBox.Show("名称无效", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!File.Exists(ProgramPath.Text))
+            {
+                MessageBox.Show("文件路径不存在", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (MessageBox.Show("确认要更改吗？", "Q", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+            {
+                return;
+            }
+            var item = StartUpItems[StartupList.SelectedIndices[0]];
+            item.Name = ProgramName.Text;
+            item.Path = ProgramPath.Text;
+            item.Arg = ProgramArg.Text;
+            item.Enabled = ProgramEnabled.Checked;
+            File.WriteAllText("Config.json", JsonConvert.SerializeObject(StartUpItems, Formatting.Indented));
+            RefreshList();
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            if (StartupList.SelectedIndices.Count == 0)
+            {
+                MessageBox.Show("请选择一项", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (MessageBox.Show("确认要删除吗？", "Q", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+            {
+                return;
+            }
+            StartUpItems.RemoveAt(StartupList.SelectedIndices[0]);
+            File.WriteAllText("Config.json", JsonConvert.SerializeObject(StartUpItems, Formatting.Indented));
+            RefreshList();
+        }
+
+        private void StartUpSelector_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!Loaded) return;
+            if (StartUpSelector.Checked)
+            {
+                AddStartupProgram("AutoStartup", Application.ExecutablePath + " -o", true);
+            }
+            else
+            {
+                AddStartupProgram("AutoStartup", Application.ExecutablePath + " -o", false);
+            }
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+                string path = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+                if (File.Exists(path))
+                {
+                    var info = new FileInfo(path);
+                    ProgramName.Text = info.Name.Replace(info.Extension, "");
+                    ProgramPath.Text = info.FullName;
+                    ProgramEnabled.Checked = true;
+                }
+            }
+        }
+
+        private void MainForm_DragLeave(object sender, EventArgs e)
+        {
+            ProgramName.Text = "";
+            ProgramPath.Text = "";
+            ProgramArg.Text = "";
+            ProgramEnabled.Checked = false;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("Config.json"))
+            {
+                StartUpItems = JsonConvert.DeserializeObject<List<StartUpItem>>(File.ReadAllText("Config.json"));
+            }
+            if (StartUpItems == null)
+            {
+                StartUpItems = new();
+            }
+            StartupList.MouseDoubleClick += StartupList_MouseDoubleClick;
+            RefreshList();
+            StartUpSelector.Checked = IsStartup;
+            Loaded = true;
+            if (!Maintance)
+            {
+                new Thread(StartProcess).Start();
+            }
+        }
+
+        private void StartupList_MouseDoubleClick(object? sender, MouseEventArgs e)
+        {
+            ListViewHitTestInfo info = StartupList.HitTest(e.X, e.Y);
+            ListViewItem item = info.Item;
+
+            if (item != null)
+            {
+                ProgramName.Text = item.SubItems[1].Text;
+                ProgramPath.Text = item.SubItems[2].Text;
+                ProgramArg.Text = item.SubItems[3].Text;
+                ProgramEnabled.Checked = item.SubItems[0].Text == "是";
+            }
+        }
+
+        private void StartProcess()
+        {
+            HasError = false;
+            StopFlag = false;
+            Invoke(new MethodInvoker(() => StopBtn.Enabled = true));
+            for (int i = 0; i < 30; i++)
+            {
+                CurrentStatus = $"将在{((double)30 - i) / 10:f2}s后开始启动程序执行...";
+                if (StopFlag)
+                {
+                    CurrentStatus = $"维护模式...";
+                    StopFlag = false;
+                    Invoke(new MethodInvoker(() => StopBtn.Enabled = false));
+                    return;
+                }
+                Thread.Sleep(100);
+            }
+
+            foreach (var item in StartUpItems.Where(x => x.Enabled))
+            {
+                if (StopFlag)
+                {
+                    CurrentStatus = $"维护模式...";
+                    StopFlag = false;
+                    Invoke(new MethodInvoker(() => StopBtn.Enabled = false));
+                    return;
+                }
+                CurrentStatus = $"启动程序 {item.Name} 中...";
+                try
+                {
+                    Process.Start(new ProcessStartInfo { FileName = item.Path, Arguments = item.Arg, UseShellExecute = true });
+                }
+                catch (Exception e)
+                {
+                    CurrentStatus = $"程序 {item.Name} 启动发生错误.";
+                    HasError = true;
+                    new Thread(() =>
+                    {
+                        MessageBox.Show($"程序 {item.Name} 启动失败，错误原因: {e.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }).Start();
+                }
+            }
+
+            if (!HasError)
+            {
+                if (TestFlag)
+                {
+                    CurrentStatus = $"测试通过，未发生错误...";
+                    Invoke(new MethodInvoker(() => StopBtn.Enabled = false));
+                    return;
+                }
+                for (int i = 0; i < 30; i++)
+                {
+                    CurrentStatus = $"启动完成...将在{((double)30 - i) / 10:f2}s后退出程序...";
+                    if (StopFlag)
+                    {
+                        CurrentStatus = $"维护模式...";
+                        StopFlag = false;
+                        Invoke(new MethodInvoker(() => StopBtn.Enabled = false));
+                        return;
+                    }
+                    Thread.Sleep(100);
+                }
+                Environment.Exit(0);
+            }
+            else
+            {
+                CurrentStatus = $"启动完成，但是部分程序启动失败，查看错误窗口获取错误信息";
+            }
+        }
+
+        private void OpenFileDialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog.ShowDialog();
+            string path = OpenFileDialog.FileName;
+            if (File.Exists(path))
+            {
+                var info = new FileInfo(path);
+                ProgramName.Text = info.Name.Replace(info.Extension, "");
+                ProgramPath.Text = info.FullName;
+                ProgramEnabled.Checked = true;
+            }
+        }
+
+        private void Program_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddBtn.PerformClick();
+            }
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            StopFlag = true;
+        }
+
+        private void TestBtn_Click(object sender, EventArgs e)
+        {
+            TestFlag = true;
+            new Thread(StartProcess).Start();
+        }
+    }
+}
