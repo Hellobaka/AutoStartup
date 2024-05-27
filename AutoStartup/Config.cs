258cbd378110b47ad9223cdbfee6912518e7b3b6
Mr.喜 <863450594@qq.com>
2024-05-27 13:21:24 +0800

    添加配置项

---
+++ AutoStartup/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AutoStartup.MainForm;
+
+namespace AutoStartup
+{
+    public class Config
+    {
+        public List<StartUpItem> StartUpItems { get; set; } = new();
+
+        public int StartWaitTime { get; set; } = 3;
+    }
+}
