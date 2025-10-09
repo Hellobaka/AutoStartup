8ee9a9c5d8b4dede2950568670a20253d70a5ef0
Mr.喜 <863450594@qq.com>
2025-10-09 17:55:50 +0800

    编辑服务UI布局

---
+++ AutoStartup/ControlWithTitle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AutoStartup
+{
+    /// <summary>
+    /// ToggleButtonWithTitle.xaml 的交互逻辑
+    /// </summary>
+    public partial class ControlWithTitle : ContentControl
+    {
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ControlWithTitle), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty TitleWidthProperty =
+            DependencyProperty.Register("TitleWidth", typeof(int), typeof(ControlWithTitle), new PropertyMetadata(80, OnTitleWidthChanged));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public int TitleWidth
+        {
+            get => (int)GetValue(TitleWidthProperty);
+            set => SetValue(TitleWidthProperty, value);
+        }
+
+        private ColumnDefinition _titleColumn;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _titleColumn = GetTemplateChild("PART_TitleColumn") as ColumnDefinition;
+            UpdateTitleWidth();
+        }
+
+        private static void OnTitleWidthChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = (ControlWithTitle)d;
+            ctrl.UpdateTitleWidth();
+        }
+
+        private void UpdateTitleWidth()
+        {
+            if (_titleColumn != null)
+            {
+                _titleColumn.Width = new GridLength(TitleWidth);
+            }
+        }
+    }
+}
--- AutoStartup/MainWindow.xaml.cs
+++ AutoStartup/MainWindow.xaml.cs
@@ -1,15 +1,11 @@
-﻿using AutoStartup.ViewModel;
+﻿using AutoStartup.Services;
+using AutoStartup.ViewModel;
 using System.IO;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 using DataFormats = System.Windows.DataFormats;
 using DragDropEffects = System.Windows.DragDropEffects;
 using DragEventArgs = System.Windows.DragEventArgs;
@@ -25,33 +21,132 @@ namespace AutoStartup
         {
             InitializeComponent();
             DataContext = new MainWindowViewModel();
+            UIDispatcher = Dispatcher.CurrentDispatcher;
+            ExtraServiceButtonMenu.AddHandler(MenuItem.ClickEvent, new RoutedEventHandler(ExtraServiceButtonMenuItem_Click));
+        }
+
+        public static Dispatcher UIDispatcher { get; private set; }
+
+        private MainWindowViewModel? ViewModel => DataContext as MainWindowViewModel;
+
+        private bool BalloonShown { get; set; }
+
+        public static void ShowError(string msg)
+        {
+            HandyControl.Controls.MessageBox.Show(msg, "Error", icon: MessageBoxImage.Error);
+        }
+
+        public static void ShowInfo(string msg)
+        {
+            HandyControl.Controls.MessageBox.Show(msg, "Info", icon: MessageBoxImage.Information);
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            if (Shared.Maintenance && !BalloonShown)
+            {
+                BalloonShown = true;
+                TaskbarHelper.ShowTrayInfo("AutoStartup", "程序已隐藏到系统托盘，双击图标可重新打开窗口。", ToolTipIcon.Info);
+            }
+
             e.Cancel = true;
             Hide();
         }
 
-        private void Window_DragOver(object sender, DragEventArgs e)
+        private void ServiceEditor_DropEnter(object sender, DragEventArgs e)
         {
-            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
-            e.Handled = true;
-        }
-
-        private void Window_Drop(object sender, DragEventArgs e)
-        {
+            ServiceEditor.IsExpanded = true;
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 var files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                var vm = DataContext as MainWindowViewModel;
-                foreach (var file in files)
+                if (files.Length != 1 || Path.GetExtension(files.FirstOrDefault()) != ".exe")
                 {
-                    if (File.Exists(file))
-                        vm.AddServiceFromFile(file);
+                    e.Effects = DragDropEffects.None;
+                    e.Handled = true;
+                    return;
+                }
+                e.Effects = DragDropEffects.Copy;
+                ViewModel!.ShowDragPreview(files.First());
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
+
+        private void ServiceEditor_DragLeave(object sender, DragEventArgs e)
+        {
+            ViewModel!.RemoveDragPreview();
+        }
+
+        private void ServiceList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            ViewModel!.PreviewService = ViewModel.SelectedService?.Service.Clone();
+            foreach (var item in e.RemovedItems)
+            {
+                if (item is ServiceViewModel vm)
+                {
+                    vm.OperationLogs.CollectionChanged -= OperationLogs_CollectionChanged;
+                    vm.ConsoleOutputs.CollectionChanged -= ConsoleOutputs_CollectionChanged;
+                }
+            }
+            foreach (var item in e.AddedItems)
+            {
+                if (item is ServiceViewModel vm)
+                {
+                    vm.OperationLogs.CollectionChanged += OperationLogs_CollectionChanged;
+                    vm.ConsoleOutputs.CollectionChanged += ConsoleOutputs_CollectionChanged;
                 }
             }
         }
 
+        private void OperationLogs_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+        }
+
+        private void ConsoleOutputs_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+        }
+
+        private void ExtraServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExtraServiceButtonMenu.PlacementTarget = ExtraServiceButton;
+            ExtraServiceButtonMenu.Placement = PlacementMode.Bottom;
+            ExtraServiceButtonMenu.IsOpen = true;
+        }
+
+        private void ExtraServiceButtonMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is not MenuItem menuItem)
+            {
+                return;
+            }
+            var tag = menuItem.Tag as string;
+            if (string.IsNullOrEmpty(tag))
+            {
+                return;
+            }
+
+            string type = $"AutoStartup.Windows.{tag}";
+            var windowType = Type.GetType(type);
+            if(windowType == null)
+            {
+                ShowError($"无法找到窗口类型 {type}");
+                return;
+            }
+            if (Activator.CreateInstance(windowType) is not Window window)
+            {
+                ShowError($"无法创建窗口 {type}");
+                return;
+            }
+            window.Owner = this;
+            window.ShowDialog();
+            if ((window.DialogResult ?? false)
+                && window.Tag is Service service)
+            {
+                ViewModel!.PreviewService = service;
+            }
+        }
     }
 }
